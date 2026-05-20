acb95cbf142fc1497c79f39955d11cdd08c9e07f
pawan <pawangrover904@gmail.com>
2026-05-20 18:18:48 +0530

    add hero contant and hero image in  hero.jsx

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,13 +1,30 @@
-import React from 'react'
+import React from "react";
+import Grocery from "../../assets/Grocery Website Assets/grocery.png";
 
 function Hero() {
   return (
     <section>
-      <div className='max-w-[1400px] mx-auto px-10 '>
-e
+      <div className="max-w-[1400px] mx-auto px-10 ">
+        {/* hero contant */}
+        <div>
+          <span className="bg-orange-100 text-orange-500 text-lg px-5 py-2 rounded-full">
+            Export Best Quality...
+          </span>
+          <h1 className="text-7xl font-bold">
+            Tasty Organic <span className="text-orange-500 ">Fruits</span> & <span className="text-orange-500 ">Vaggies</span> in your city
+          </h1>
+          <p className="text-zinc-600 text-lg">
+            Bred for a high content of beneficial substances. Our products are all fresh and healthy.
+          </p>
+        </div>
+
+        {/* hero image */}
+        <div>
+          <img src={Grocery} alt="hero image" />
+        </div>
       </div>
     </section>
-  )
+  );
 }
 
-export default Hero
+export default Hero;
