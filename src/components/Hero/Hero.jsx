ad25c712372e55e3b14a394d9cbd7f3c4f148ecb
pawan <pawangrover904@gmail.com>
2026-05-20 18:57:59 +0530

    add button folder and button.jsx and set alignment hero contant and hero image

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,25 +1,27 @@
 import React from "react";
 import Grocery from "../../assets/Grocery Website Assets/grocery.png";
+import Button from "../Button/Button";
 
 function Hero() {
   return (
     <section>
-      <div className="max-w-[1400px] mx-auto px-10 ">
+      <div className="max-w-[1400px] mx-auto px-10 flex items-center ">
         {/* hero contant */}
-        <div>
+        <div className="flex-1">
           <span className="bg-orange-100 text-orange-500 text-lg px-5 py-2 rounded-full">
             Export Best Quality...
           </span>
-          <h1 className="text-7xl font-bold">
-            Tasty Organic <span className="text-orange-500 ">Fruits</span> & <span className="text-orange-500 ">Vaggies</span> in your city
+          <h1 className="text-7xl/20 font-bold mt-4">
+            Tasty Organic <span className="text-orange-500 ">Fruits</span> & <span className="text-orange-500 ">Vaggies</span> <br />in your city
           </h1>
-          <p className="text-zinc-600 text-lg">
+          <p className="text-zinc-600 text-lg max-w-[530px] mt-5 mb-10">
             Bred for a high content of beneficial substances. Our products are all fresh and healthy.
           </p>
+          <Button content="Shop Now"/>
         </div>
 
         {/* hero image */}
-        <div>
+        <div className="flex-1">
           <img src={Grocery} alt="hero image" />
         </div>
       </div>
