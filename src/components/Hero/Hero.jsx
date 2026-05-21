c8974f66de60472310ea280b5eb17e1cc45beb95
pawan <pawangrover904@gmail.com>
2026-05-21 11:55:50 +0530

    fix text size in hero.jsx and make hero contant response md sm size

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -5,13 +5,13 @@ import Button from "../Button/Button";
 function Hero() {
   return (
     <section>
-      <div className="min-h-screen max-w-[1400px]   mx-auto px-10 flex items-center pt-25 ">
+      <div className="min-h-screen max-w-[1400px]   mx-auto px-10 flex md:flex-row flex-col items-center pt-25 ">
         {/* hero contant */}
         <div className="flex-1">
           <span className="bg-orange-100 text-orange-500 text-lg px-5 py-2 rounded-full">
             Export Best Quality...
           </span>
-          <h1 className="text-7xl/20 font-bold mt-4">
+          <h1 className="md:text-7xl/20 text-5xl/14 font-bold mt-4">
             Tasty Organic <span className="text-orange-500 ">Fruits</span> & <span className="text-orange-500 ">Vaggies</span> <br />in your city
           </h1>
           <p className="text-zinc-600 text-lg max-w-[530px] mt-5 mb-10">
