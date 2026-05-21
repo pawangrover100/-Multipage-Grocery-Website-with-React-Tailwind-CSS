86fc70f04a0ecef9b443acbeff40e86e81e62deb
pawan <pawangrover904@gmail.com>
2026-05-21 12:08:27 +0530

    change text size in Navbar.jsx line no 17 and change button change button size in button.jsx line no 5

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -14,7 +14,7 @@ function Hero() {
           <h1 className="md:text-7xl/20 text-5xl/14 font-bold mt-4">
             Tasty Organic <span className="text-orange-500 ">Fruits</span> & <span className="text-orange-500 ">Vaggies</span> <br />in your city
           </h1>
-          <p className="text-zinc-600 text-lg max-w-[530px] mt-5 mb-10">
+          <p className="text-zinc-600 md:text-lg  text:md max-w-[530px] mt-5 mb-10">
             Bred for a high content of beneficial substances. Our products are all fresh and healthy.
           </p>
           <Button content="Shop Now"/>
