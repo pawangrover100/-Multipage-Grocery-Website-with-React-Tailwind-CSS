8d7401e7d1bf210e42df41a1d1947ab5e3d64f00
pawan <pawangrover904@gmail.com>
2026-05-21 18:24:49 +0530

    make mobile menu and fix bugg

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,28 +1,34 @@
-import React from "react";
+import { useState } from "react";
 import { IoHeartSharp } from "react-icons/io5";
 import { HiShoppingBag } from "react-icons/hi2";
 import { IoSearch } from "react-icons/io5";
 import { TbMenu2 } from "react-icons/tb";
 
 function Navbar() {
+  const [showMenu, setShowMenu] = useState(false);
+
+  const toggleMenu = () => {
+    setShowMenu((prev) => !prev)
+  };
+
   return (
     <header className="bg-white fixed top-0 right-0 left-0 ">
       <nav className="max-w-[1400px] mx-auto px-10 md:h-[14vh] h-[12vh] items-center flex justify-between">
         {/* logo */}
-        <a href="" className="text-3xl font-semibold">
+        <a href="#" className="text-3xl font-semibold">
           Gr<span className="text-orange-400  uppercase">o</span>cify
         </a>
 
         {/* Desktop menu */}
-        <ul className="md:flex items-center gap-x-15 hidden">
+        <ul className="md:flex items-center gap-x-10 hidden">
           <li>
-            <a href="" className="font-semibold tracking-wider text-orange-500">
+            <a href="#" className="font-semibold tracking-wider text-orange-500">
               Home
             </a>
           </li>
           <li>
             <a
-              href=""
+              href="#"
               className="font-semibold tracking-wider text-zinc-800 hover:text-orange-500"
             >
               About us
@@ -30,7 +36,7 @@ function Navbar() {
           </li>
           <li>
             <a
-              href=""
+              href="#"
               className="font-semibold tracking-wider text-zinc-800  hover:text-orange-500"
             >
               Process
@@ -38,7 +44,7 @@ function Navbar() {
           </li>
           <li>
             <a
-              href=""
+              href="#"
               className="font-semibold tracking-wider text-zinc-800  hover:text-orange-500"
             >
               Contact us
@@ -63,18 +69,71 @@ function Navbar() {
             </button>
           </div>
 
-          <a href="" className="text-2xl text-zinc-800">
+          <a href="#" className="text-2xl text-zinc-800">
             <IoHeartSharp />
           </a>
-          <a href="" className="text-2xl text-zinc-800">
+          <a href="#" className="text-2xl text-zinc-800">
             <HiShoppingBag />
           </a>
 
           {/* hamburgger */}
-          <a href="" className="text-zinc-800  text-3xl md:hidden">
+          <button
+            className="text-zinc-800 text-3xl md:hidden"
+            onClick={toggleMenu}
+          >
             <TbMenu2 />
-          </a>
+          </button>
         </div>
+
+        {/* mobile menu */}
+        <ul
+          className={`z-50 flex flex-col gap-y-10 bg-orange-500/15 backdrop-blur-xl rounded-xl p-10 items-center gap-x-12 md:hidden absolute top-[120px] transition-all duration-500 ${
+            showMenu ? "left-1/2 -translate-x-1/2" : "-left-full"
+          }`}
+        >
+          <li>
+            <a href="#" className="font-semibold tracking-wider text-orange-500">
+              Home
+            </a>
+          </li>
+          <li>
+            <a
+              href="#"
+              className="font-semibold tracking-wider text-zinc-800 hover:text-orange-500"
+            >
+              About us
+            </a>
+          </li>
+          <li>
+            <a
+              href="#"
+              className="font-semibold tracking-wider text-zinc-800  hover:text-orange-500"
+            >
+              Process
+            </a>
+          </li>
+          <li>
+            <a
+              href="#"
+              className="font-semibold tracking-wider text-zinc-800  hover:text-orange-500"
+            >
+              Contact us
+            </a>
+          </li>
+          <li className="flex md:hidden p-1 border-2 border-orange-500 rounded-full">
+            <input
+              type="text"
+              name="text"
+              id="text"
+              placeholder="Search..."
+              autoComplete="off"
+              className="flex-1 h-[5vh] px-3 focus:outline-none"
+            />
+            <button className="bg-gradient-to-b from-red-500 to-orange-500 text-white rounded-full w-10 h-10  flex justify-center items-center text-xl ">
+              <IoSearch />
+            </button>
+          </li>
+        </ul>
       </nav>
     </header>
   );
