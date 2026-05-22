dd71e1439d39b43d26595df83520ed1fc688faaf
pawan <pawangrover904@gmail.com>
2026-05-22 11:08:12 +0530

    create Heading folder and heading.jsx and make Category section headline (shop by Category) and export Heading.jsx in Category.jsx

--- src/components/Category/Category.jsx
+++ src/components/Category/Category.jsx
@@ -1,13 +1,14 @@
-import React from 'react'
+import React from "react";
+import Heading from "../Heading/Heading";
 
 function Category() {
   return (
-   <section>
-    <div className='py-20'>
-        
-    </div>
-   </section>
-  )
+    <section>
+      <div className="py-20">
+      <Heading />
+      </div>
+    </section>
+  );
 }
 
-export default Category
+export default Category;
---
+++ src/components/Heading/Heading.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Heading() {
+  return (
+    <div className="w-fit mx-auto">
+      <h2 className="md:text-5xl text-[2.5rem] font-bold ">
+        
+        <span className="text-orange-500">Shop</span> By Category
+      </h2>
+      <div className="w-34 md:mt-6 mt-3 h-1 bg-orange-500 ml-auto"></div>
+    </div>
+  );
+}
+
+export default Heading;
