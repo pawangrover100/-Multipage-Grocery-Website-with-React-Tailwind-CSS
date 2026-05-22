84507b0287533921f25447805a16844fae546062
pawan <pawangrover904@gmail.com>
2026-05-22 18:42:06 +0530

    make values section responsive

--- src/components/Values/Values.jsx
+++ src/components/Values/Values.jsx
@@ -33,13 +33,13 @@ function Values() {
 
   const LeftValues = values.slice(0, 2).map((item) => {
     return (
-      <div className="flex flex-row-reverse items-center gap-7">
+      <div className="flex md:flex-row-reverse items-center gap-7">
         <div>
           <span className="flex justify-center items-center text-3xl text-white bg-gradient-to-b form-orange-400 to-orange-500 w-15 h-15 rounded-full">
             {item.icon}
           </span>
         </div>
-        <div className="text-right">
+        <div className="md:text-right">
           <h3 className="text-zine-800 font-bold text-3xl ">{item.title}</h3>
           <p className="text-zine-600 mt-2">{item.para}</p>
         </div>
@@ -65,16 +65,16 @@ function Values() {
   return (
     <div className="max-w-[1400px] mx-auto px-10 py-20">
       <Heading highlight="Our" heading="Values" />
-      <div className="flex gap-5 mt-15">
+      <div className="flex md:flex-row flex-col gap-15 md:gap-5 mt-15">
         {/* left values */}
-        <div className="min-h-100 flex  flex-col  justify-between">
+        <div className="md:min-h-100 gap-15 flex  flex-col  justify-between">
           {LeftValues}
         </div>
-        <div className="w-1/2">
+        <div className=" md:flex w-1/2 hidden">
           <img src={Basket} alt="" />
         </div>
         {/* right value */}
-        <div className="min-h-100 flex  flex-col  justify-between">
+        <div className="md:min-h-100 gap-15 flex  flex-col  justify-between">
           {RightValues}
         </div>
       </div>
