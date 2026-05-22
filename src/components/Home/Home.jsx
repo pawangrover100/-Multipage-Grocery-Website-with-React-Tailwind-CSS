2d8bcc43316f18dbb2d62c743e3e185ac097b2b2
pawan <pawangrover904@gmail.com>
2026-05-22 16:49:39 +0530

    add Values heading and export default in home.jsx

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import Navbar from '../Navbar/Navbar'
 import Hero from '../Hero/Hero'
 import Category from '../Category/Category'
+import Values from '../Values/Values'
 
 function Home() {
   return (
     <div><Navbar />
     <Hero />
     <Category />
+    <Values />
     </div>
 
   )
--- src/components/Values/Values.jsx
+++ src/components/Values/Values.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
-
+import Heading from '../Heading/Heading.jsx'
 function Values() {
   return (
-    <div>Values</div>
+    <div className='max-w-[1400px] mx-auto px-10'>
+        <Heading highlight="Our" heading="Values"/>
+    </div>
   )
 }
 
