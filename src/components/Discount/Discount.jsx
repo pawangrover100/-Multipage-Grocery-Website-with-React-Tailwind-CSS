80a129d0012dd654bd1fa93a580573bbec16536e
pawan <pawangrover904@gmail.com>
2026-05-24 16:13:40 +0530

    responsive Discount section

--- src/components/Discount/Discount.jsx
+++ src/components/Discount/Discount.jsx
@@ -4,10 +4,10 @@ import FreshFruits from "../../assets/Grocery Website Assets/fresh-fruits.png"
 function Discount() {
   return (
     <section className="bg-zinc-100 bg-no-repeat bg-contain bg-right" style={{backgroundImage:`url(${FreshFruits})`}}>
-      <div className="max-w-[1400pz] mx-auto px-10 flex py1-10">
-        <span className="text-9xl text-orange-500 font-bold traform -rotate-90 h-fit self-center">20%</span>
+      <div className="md:bg-transparent bg-zinc-100 max-w-[1400pz] mx-auto px-10 flex md:flex-row flex-col py1-10">
+        <span className="md:text-9xl text-5xl  text-orange-500 font-bold traform md:-rotate-90 h-fit md:self-center">20%</span>
         <div className="max-w-[700px]">
-          <h3 className="text-7xl text-zinc-800 font-bold">First Order Discount!</h3>
+          <h3 className="md:text-7xl text-4xl text-zinc-800 font-bold mt-2">First Order Discount!</h3>
           <p className="text-zinc-600 my-6 "> 
             Enjoy an exclusive first order discount on our grocery website! Shop
             fresh essentials and save big on your first purchase. Fast delivery
