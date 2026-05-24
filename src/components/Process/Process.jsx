5741a8ae9c2b82bd4c635f6b944111f7f480e12b
pawan <pawangrover904@gmail.com>
2026-05-24 16:35:25 +0530

    movr heading left side

--- src/components/Process/Process.jsx
+++ src/components/Process/Process.jsx
@@ -5,7 +5,9 @@ function Process() {
   return (
     <section>
       <div className="max-w-[1400px] mx-auto px-10 py-20">
-        <Heading highlight="Our" heading="Process"/>
+     <div className="w-fit mr-auto">
+           <Heading highlight="Our" heading="Process"/>
+     </div>
       </div>
     </section>
   );
