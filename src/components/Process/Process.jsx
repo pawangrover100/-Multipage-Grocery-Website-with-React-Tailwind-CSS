b9c1291a2139e67bab836692211468f52cafe29b
pawan <pawangrover904@gmail.com>
2026-05-25 12:18:17 +0530

    make Process section

--- src/components/Process/Process.jsx
+++ src/components/Process/Process.jsx
@@ -1,16 +1,77 @@
 import React from "react";
 import Heading from "../Heading/Heading";
+import {
+  TbCircleNumber1Filled,
+  TbCircleNumber2Filled,
+  TbCircleNumber3Filled,
+  TbCircleNumber4Filled,
+} from "react-icons/tb";
+
+import { PiFactory, PiPlant } from "react-icons/pi";
+import { SlBadge } from "react-icons/sl";
+import { BsTruck } from "react-icons/bs";
 
 function Process() {
+  const renderStep = steps.map((item) => {
+    return (
+      <div key={item.id} className={`flex-1 ${item.id% 2 ===0 ? '-mt-100': ''}`}>
+        <span className="flex justify-center items-center w-18 h-18 mx-auto text-8xl rounded-full  text-white bg-zinc-800 outline-dashed outline-zinc-800 outline-offset-7 outline-[3px]">
+          {item.number}
+        </span>
+        <div className="flex items-center gap-x-5 mt-10 ">
+          <span className="  w-15 h-15 flex justify-center items-center  text-3xl bg-gradient-to-b from-orange-400 to-orange-600 text-white  rounded-full">{item.icon}</span>
+
+          <div className="flex-1" >
+            <h4 className="text-zinc-800 text-2xl font-bold">{item.title}</h4>
+            <p className="text-zinc-600 mt-2">{item.para}</p>
+          </div>
+        </div>
+      </div>
+    );
+  });
+
   return (
     <section>
       <div className="max-w-[1400px] mx-auto px-10 py-20">
-     <div className="w-fit mr-auto">
-           <Heading highlight="Our" heading="Process"/>
-     </div>
+        <div className="w-fit mr-auto">
+          <Heading highlight="Our" heading="Process" />
+        </div>
+
+        <div className="flex justify-center items-center mt-20 pt-50">{renderStep}</div>
       </div>
     </section>
   );
 }
 
 export default Process;
+
+const steps = [
+  {
+    id: 1,
+    number: <TbCircleNumber1Filled />,
+    title: "Sourcing",
+    para: "It is a long established fact that a reader",
+    icon: <PiPlant />,
+  },
+  {
+    id: 2,
+    number: <TbCircleNumber2Filled />,
+    title: "Manufacturing",
+    para: "It is a long established fact that a reader",
+    icon: <PiFactory />,
+  },
+  {
+    id: 3,
+    number: <TbCircleNumber3Filled />,
+    title: "Quality Control",
+    para: "It is a long established fact that a reader",
+    icon: <SlBadge />,
+  },
+  {
+    id: 4,
+    number: <TbCircleNumber4Filled />,
+    title: "Logistics",
+    para: "It is a long established fact that a reader",
+    icon: <BsTruck />,
+  },
+];
