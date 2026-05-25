d4c34a6f3a00d6efafbbcbb02cc9a0b9f6ed92a1
pawan <pawangrover904@gmail.com>
2026-05-25 13:04:32 +0530

    add padding in testimonials section heading

--- src/components/Testimonials/Testimonials.jsx
+++ src/components/Testimonials/Testimonials.jsx
@@ -3,7 +3,7 @@ import Heading from '../Heading/Heading.jsx'
 function Testimonials() {
   return (
    <section>
-    <div className='max-w-[1400px] px-10 mx-auto'>
+    <div className='max-w-[1400px] px-10 mx-auto py-20'>
         <Heading highlight='Customers' heading="Saying" />
     </div>
    </section>
