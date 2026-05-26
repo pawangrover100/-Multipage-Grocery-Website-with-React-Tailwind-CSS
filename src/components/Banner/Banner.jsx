41150882d565cb4b872a67ad4761b409b826cee5
pawan <pawangrover904@gmail.com>
2026-05-26 14:29:22 +0530

    overlay banner in Banner.jsx

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,12 +1,13 @@
 function Banner({ title, bgimage }) {
   return (
     <div
-      className=" h-[50vh] mt-25 flex justify-center items-center bg-cover bg-center"
+      className=" h-[50vh] mt-25 flex justify-center items-center bg-cover bg-center relative"
       style={{ backgroundImage: `url(${bgimage})` }}
     >
-      <h5 className="text-5xl text-zinc-800 bg-white p-5 rounded-xl font-bold">
+      <h5 className="text-5xl text-zinc-800 bg-white p-5 rounded-xl font-bold z-10">
         {title}
       </h5>
+      <div className="bg-black/50 absolute inset-0"></div>
     </div>
   );
 }
