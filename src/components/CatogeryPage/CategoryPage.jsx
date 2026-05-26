6c114192f04beb1fd3f039f25d62ed13e9dbce3c
pawan <pawangrover904@gmail.com>
2026-05-26 14:23:17 +0530

    add bgimage in fruits.jsx

--- src/components/CatogeryPage/CategoryPage.jsx
+++ src/components/CatogeryPage/CategoryPage.jsx
@@ -2,7 +2,7 @@ import Banner from "../Banner/Banner";
 import ProductsList from "../ProductsList/ProductsList";
 import Cards from "../Cards/Cards";
 
-function CategoryPage() {
+function CategoryPage({ title, bgimage }) {
   const renderProducts = ProductsList.map((product) => {
     return (
       <Cards
@@ -11,12 +11,13 @@ function CategoryPage() {
         image={product.image}
         name={product.name}
         price={product.price}
+        
       />
     );
   });
   return (
     <div>
-      <Banner />
+      <Banner title={title} bgimage={bgimage} />
 
       <div className="grid grid-cols-1 md:grid-cols-4 gap-9 py-20 max-w-[1400px] mx-auto px-10">
         {renderProducts}
--- src/components/Fruits/Fruits.jsx
+++ src/components/Fruits/Fruits.jsx
@@ -1,9 +1,11 @@
 import React from "react";
-import CategoryPage from "../CatogeryPage/CategoryPage.jsx"
+import CategoryPage from "../CatogeryPage/CategoryPage.jsx";
+import bgFruits from "../../assets/Grocery Website Assets/fruits-banner.jpg";
+
 function Fruits() {
   return (
     <div>
-      <CategoryPage title="Fruits and Veggies" />
+      <CategoryPage title="Fruits and Veggies" bgimage={bgFruits} />
     </div>
   );
 }
