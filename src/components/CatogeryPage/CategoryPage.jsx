4792a0d9c688e1d101432c1d5b08c5dd984c9460
pawan <pawangrover904@gmail.com>
2026-05-26 12:54:08 +0530

    create CatogeryPage.jsx

---
+++ src/components/CatogeryPage/CategoryPage.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Banner from '../Banner/Banner'
+import ProductsList from '../ProductsList/ProductsList.jx'
+function CategoryPage() {
+  return (
+    <div><Banner /></div>
+  )
+}
+
+export default CategoryPage
